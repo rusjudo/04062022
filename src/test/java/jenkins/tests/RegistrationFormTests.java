0b0db5795e98a34357197ede8abb1c7f875312c7
Ruslan Safiullin <safiullin@ymail.com>
2022-06-05 12:48:05 +0300

    add

---
+++ src/test/java/jenkins/tests/RegistrationFormTests.java
@@ -0,0 +1,63 @@
+package jenkins.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+
+@Tag("demoqa")
+public class RegistrationFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+
+    void fillFormTest() {
+        step("Open Reg Form", () -> {
+                    open("/automation-practice-form");
+                    $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+                    executeJavaScript("$('footer').remove()");
+                    executeJavaScript("$('#fixedban').remove()");
+                });
+        step("Fill Reg Form", () -> {
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("1231231230");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        //$("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#currentAddress").setValue("Some street 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        });
+        step("Check Reg Form", () -> {
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex Egorov"), text("alex@egorov.com"), text("Other"));
+        });
+    }
+    @AfterEach
+    void setEnd() {
+        closeWebDriver();
+    }
+}
